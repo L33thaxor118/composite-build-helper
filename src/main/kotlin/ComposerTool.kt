47d710aa18413ade815020239d44d635502521ff
Azriel Alvarado <alvarado_118@yahoo.com>
2022-06-26 15:58:23 -0500

    Re-open terminal sessions instead of always opening new one

--- src/main/kotlin/ComposerTool.kt
+++ src/main/kotlin/ComposerTool.kt
@@ -1,15 +1,22 @@
 import com.intellij.openapi.fileChooser.FileChooserDescriptor
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.TextFieldWithBrowseButton
+import com.intellij.openapi.wm.ToolWindowManager
 import com.intellij.ui.BooleanTableCellRenderer
+import com.intellij.ui.content.Content
+import com.intellij.ui.content.ContentManager
 import com.intellij.ui.table.JBTable
 import org.jdesktop.swingx.JXTable
+import org.jetbrains.plugins.terminal.ShellTerminalWidget
+import org.jetbrains.plugins.terminal.TerminalToolWindowFactory
 import org.jetbrains.plugins.terminal.TerminalView
+import org.jetbrains.plugins.terminal.arrangement.TerminalWorkingDirectoryManager
 import java.awt.FlowLayout
 import java.awt.event.MouseEvent
 import java.awt.event.MouseListener
 import java.io.File
 import java.io.IOException
+import java.util.*
 import javax.swing.*
 
 
@@ -86,14 +93,45 @@ class ComposerTool(private val project: Project) {
     private fun findProjects(path: String): List<File> =
         File(path).walk().filter { it.isDirectory }.filter { File(it.path, "settings.gradle").exists() }.toList()
 
-    fun openTerminal(path: String, tabName: String) {
+    fun openTerminal(workingDirectory: String, tabName: String) {
         //based on this: https://github.com/JetBrains/intellij-community/blob/95ab6a1ecfdf49754f7eb5a81984cdc2c4fa0ca5/plugins/sh/src/com/intellij/sh/run/ShTerminalRunner.java
         //looks like beyond plugin extensions, we can use classes defined in other plugins as well. In this case, we use TerminalView from intelliJs Terminal plugin.
         val terminalView = TerminalView.getInstance(project)
+        val window = ToolWindowManager.getInstance(project).getToolWindow(TerminalToolWindowFactory.TOOL_WINDOW_ID) ?: return
+        val contentManager = window.contentManager
+        val pair = getSuitableProcess(contentManager, workingDirectory)
         try {
-            terminalView.createLocalShellWidget(path, tabName)
+            if (pair == null) {
+                terminalView.createLocalShellWidget(workingDirectory, tabName)
+                return
+            }
+            window.activate(null)
+            contentManager.setSelectedContent(pair.first!!)
         } catch (e: IOException) {
 
+        }
+    }
+
+    private fun getSuitableProcess(contentManager: ContentManager, workingDirectory: String): Pair<Content?, ShellTerminalWidget?>? {
+        val selectedContent = contentManager.selectedContent
+        if (selectedContent != null) {
+            val pair = getSuitableProcess(selectedContent, workingDirectory)
+            if (pair != null) return pair
+        }
+        return Arrays.stream(contentManager.contents)
+            .map { content -> getSuitableProcess(content, workingDirectory) }
+            .filter(Objects::nonNull)
+            .findFirst()
+            .orElse(null)
+    }
+
+    private fun getSuitableProcess(content: Content, workingDirectory: String): Pair<Content?, ShellTerminalWidget?>? {
+        val widget = TerminalView.getWidgetByContent(content) as? ShellTerminalWidget ?: return null
+        val currentWorkingDirectory = TerminalWorkingDirectoryManager.getWorkingDirectory(widget, null)
+        return if (currentWorkingDirectory == workingDirectory) {
+            Pair(content, widget)
+        } else {
+            null
         }
     }
 }
